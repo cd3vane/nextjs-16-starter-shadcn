2fc9ec2358e300ea51677e723631a359223acc37
cd3vane <devane.charles98@gmail.com>
2025-11-22 10:07:24 -0500

    move files and set up new projects

---
+++ services/PlaylistService/Program.cs
@@ -0,0 +1,38 @@
+﻿using EventHub.Hubs;
+using EventHub.Services;
+using EventHub.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load config
+builder.Services.Configure<RabbitMqConfig>(builder.Configuration.GetSection("RabbitMQ"));
+
+// Add SignalR
+builder.Services.AddSignalR();
+
+// Add RabbitMqListener & MessageDispatcher as singletons
+builder.Services.AddSingleton<MessageDispatcher>();
+builder.Services.AddHostedService<RabbitMqListener>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+
+
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+app.MapHub<MessageHub>("/messages");
+
+app.Run();
---
+++ services/PlaylistService/Services/MessageDispatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using EventHub.Hubs;
+using EventHub.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace EventHub.Services;
+
+public class MessageDispatcher(ILogger<MessageDispatcher> logger, IHubContext<MessageHub> hubContext)
+{
+    public async Task DispatchAsync(Message message)
+    {
+        logger.LogInformation("Dispatching message: {Message}", message.Text);
+        await hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}

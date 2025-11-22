792289b301526a35c0c06bf3e0b346e721b6a8e3
cd3vane <devane.charles98@gmail.com>
2025-11-22 11:22:33 -0500

    login working

--- frontend/playlist-ui/lib/providers/spotify.ts
+++ frontend/playlist-ui/lib/providers/spotify.ts
@@ -8,4 +8,24 @@ export const auth = betterAuth({
             redirectURI: process.env.SPOTIFY_REDIRECT_URI as string,
         },
     },
+    session: {
+        modelName: "sessions",
+        fields: {
+            userId: "user_id"
+        },
+        expiresIn: 604800, // 7 days
+        updateAge: 86400, // 1 day
+        disableSessionRefresh: true, // Disable session refresh so that the session is not updated regardless of the `updateAge` option. (default: `false`)
+        additionalFields: { // Additional fields for the session table
+            customField: {
+                type: "string",
+            }
+        },
+        storeSessionInDatabase: false, // Store session in database when secondary storage is provided (default: `false`)
+        preserveSessionInDatabase: false, // Preserve session records in database when deleted from secondary storage (default: `false`)
+        cookieCache: {
+            enabled: true, // Enable caching session in cookie (default: `false`)	
+            maxAge: 300 // 5 minutes
+        }
+    },
 })
